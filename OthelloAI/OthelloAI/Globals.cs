00816fb429f323039f4177e60a3321344cd0b034
Mohanad Ismail <mohanadismail789@gmail.com>
2023-04-26 18:08:53 +0200

    Add game logic

---
+++ OthelloAI/OthelloAI/Globals.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAI
+{
+    enum Player
+    {
+        None,
+        Black,
+        White
+    }
+
+    internal class Coordinate
+    {
+        public readonly int x;
+        public readonly int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static readonly List<Coordinate> directions = new()
+        {
+            new Coordinate(0, 1),
+            new Coordinate(0, -1),
+            new Coordinate(1, 0),
+            new Coordinate(-1, 0),
+            new Coordinate(1, 1),
+            new Coordinate(1, -1),
+            new Coordinate(-1, 1),
+            new Coordinate(-1, -1)
+        };
+
+        public bool isWithinBoard()
+        {
+            return x >= 0 && x < 8 && y >= 0 && y < 8;
+        }
+
+        /// <summary>
+        /// This function returns the other player
+        /// </summary>
+        /// <param name="turn">the current player</param>
+        public static Player otherPlayer(Player turn)
+        {
+            if (turn == Player.Black)
+            {
+                return Player.White;
+            }
+            else if (turn == Player.White)
+            {
+                return Player.Black;
+            }
+            else
+            {
+                return Player.None;
+            }
+        }
+
+        /// <summary>
+        /// This function is used to solve hashing collisions
+        /// </summary>
+        public override bool Equals(object? obj)
+        {
+            return obj is Coordinate coordinate &&
+                   x == coordinate.x &&
+                   y == coordinate.y;
+        }
+
+        /// <summary>
+        /// This function is used so that coordinates of the same x and y values are considered equal
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
+        }
+
+        public static Coordinate operator +(Coordinate a, Coordinate b)
+        {
+            return new Coordinate(a.x + b.x, a.y + b.y);
+        }
+
+
+    }
+
+    /// <summary>
+    /// This class represents a node in the game search tree. It contains a state, all valid next states, and a reference to the parent node.
+    /// </summary>
+    internal class StateNode
+    {
+        public State state;
+        public List<StateNode>? validNextStates;
+        public StateNode? parentNode;
+
+        public StateNode(State state, StateNode? parentNode = null)
+        {
+            this.state = state;
+            validNextStates = null;
+            this.parentNode = parentNode;
+        }
+
+        /// <summary>
+        /// This function populates the children nodes of the current state node only on request.
+        /// </summary>
+        /// <param name="turn">the player's turn for which the next states should be generated</param>
+        public void generateValidNextStates(Player turn)
+        {
+            validNextStates = new List<StateNode>();
+            Dictionary<Coordinate, List<Coordinate>> validMoves = this.state.getValidMoves(turn);
+            // iterate on all valid moves
+            foreach (KeyValuePair<Coordinate, List<Coordinate>> move in validMoves)
+            {
+                State newState = new State((Player[,])this.state.board.Clone());
+                // place the current player's piece
+                newState.board[move.Key.x, move.Key.y] = turn;
+                // flip all pieces corresponding to this move
+                foreach (Coordinate flippedPiece in move.Value)
+                {
+                    newState.board[flippedPiece.x, flippedPiece.y] = turn;
+                }
+                validNextStates.Add(new StateNode(newState, this));
+            }
+        }
+    }
+}
---
+++ OthelloAI/OthelloAI/State.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAI
+{
+    internal class State
+    {
+        public readonly Player[,] board;
+        public readonly int blackScore;
+        public readonly int whiteScore;
+
+        public State(Player[,] board)
+        {
+            this.board = board;
+            blackScore = 0;
+            whiteScore = 0;
+            foreach (Player piece in board)
+            {
+                if (piece == Player.Black)
+                {
+                    blackScore++;
+                }
+                else if (piece == Player.White)
+                {
+                    whiteScore++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function returns a dictionary of all valid moves for the given player
+        /// </summary>
+        /// <param name="turn">the player whose turn is this</param>
+        /// <returns>A dictionary where the key is a valid move, and the value is a list of coordinates of piece that would be flipped if this move is made</returns>
+        public Dictionary<Coordinate, List<Coordinate>> getValidMoves(Player turn)
+        {
+            Dictionary<Coordinate, List<Coordinate>> validMoves = new Dictionary<Coordinate, List<Coordinate>>();
+            // iterate through all the squares on the board
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (board[i, j] == Player.None)
+                    {
+                        List<Coordinate> flippedPieces = new();
+                        // iterate through all eight directions and check if this direction flips any pieces
+                        foreach (Coordinate direction in Coordinate.directions)
+                        {
+                            Coordinate current = new Coordinate(i, j);
+                            current = current + direction;
+                            while (current.isWithinBoard() && board[current.x, current.y] == Coordinate.otherPlayer(turn))
+                            {
+                                flippedPieces.Add(current);
+                                current = current + direction;
+                            }
+                            if (current.isWithinBoard() && board[current.x, current.y] == turn && flippedPieces.Count > 0)
+                            {
+                                validMoves.Add(new Coordinate(i, j), flippedPieces);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return validMoves;
+        }
+
+        /// <summary>
+        /// This function returns a new state with the given move applied
+        /// </summary>
+        /// <param name="turn">the player that wants to make this move</param>
+        /// <param name="move">the coordinate of the piece the player wants to place</param>
+        /// <returns>the new state after the move is applied and all flips are made</returns>
+        /// <exception cref="KeyNotFoundException">Thrown when the move is invalid for this player</exception>
+        public State applyMove(Player turn, Coordinate move)
+        {
+            Player[,] newBoard = (Player[,])board.Clone();
+            newBoard[move.x, move.y] = turn;
+            foreach (Coordinate flippedPiece in getValidMoves(turn)[move])
+            {
+                newBoard[flippedPiece.x, flippedPiece.y] = turn;
+            }
+            return new State(newBoard);
+        }
+    }
+}
