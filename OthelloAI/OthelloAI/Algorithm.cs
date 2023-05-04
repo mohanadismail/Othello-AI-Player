974a350e73f15826b54813664848e04d1d06b3e9
Mohanad Ismail <mohanadismail789@gmail.com>
2023-05-04 20:51:52 +0300

    Add abstract Algorithm and Heuristic classes

---
+++ OthelloAI/OthelloAI/Algorithm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAI
+{
+    internal abstract class Algorithm
+    {
+        List<Heuristic> heuristics;
+
+        public Algorithm(List<Heuristic> heuristics)
+        {
+            this.heuristics = heuristics;
+        }
+
+        public abstract State performNextMove(Player turn, StateNode node, int maxDepth);
+    }
+}
---
+++ OthelloAI/OthelloAI/Heuristic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAI
+{
+    internal abstract class Heuristic
+    {
+        int weight;
+
+        public Heuristic(int weight)
+        {
+            this.weight = weight;
+        }
+
+        public abstract int calculateUtility(State board, Player max, Player min);
+    }
+}
