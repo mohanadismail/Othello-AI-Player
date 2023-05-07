92e13343b817485d7a414407154130857bc02170
Kamel <mario.kamel@dell.com>
2023-05-07 20:17:09 +0300

    Add buttons and trackbars Add buttons and trackbars and their basic behaviors

--- OthelloAI/OthelloAI/Form1.cs
+++ OthelloAI/OthelloAI/Form1.cs
@@ -11,5 +11,56 @@ namespace OthelloAI
         {
 
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //give button1 a blue border with 2px width
+            button1.FlatAppearance.BorderColor = Color.Blue;
+            button1.FlatAppearance.BorderSize = 2;
+
+            //hide label1 and label2
+            label1.Hide();
+            label2.Hide();
+            //hide trackbar1 and trackbar2
+            trackBar1.Hide();
+            trackBar2.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //show label1 and trackbar1
+            label1.Show();
+            trackBar1.Show();
+            //hide label2 and trackbar2
+            label2.Hide();
+            trackBar2.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //show label2 and trackbar2
+            label2.Show();
+            trackBar2.Show();
+        }
     }
 }
