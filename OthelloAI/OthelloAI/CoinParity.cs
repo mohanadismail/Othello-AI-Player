e39ebc44024cd5b70b25b247d31549cd4804ad1a
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2023-05-15 00:22:08 +0300

    coin parity + actual mobility

---
+++ OthelloAI/OthelloAI/CoinParity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OthelloAI
+{
+    internal class CoinParity:Heuristic
+    {
+        public CoinParity(int weight) : base(weight)
+        {
+        }
+        public override int calculateUtility(State board, Player max, Player min)
+        {
+            int whiteScore = board.whiteScore;
+            int blackScore = board.blackScore;
+            int coinParity = 100* Math.Abs(whiteScore - blackScore)/(whiteScore+blackScore);         
+            return coinParity;
+        }
+    }
+    
+}
